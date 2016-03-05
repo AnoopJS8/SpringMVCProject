a3f92d495670799d85033f0f553f9cb4f1259fa6
AnoopJS8 <anoop.jyoti88@gmail.com>
2016-03-05 16:43:28 -0700

    Adding Maps to xml structure and PathVariables

--- SpringMVCProject/src/com/springs/controller/AnnotatedHelloController.java
+++ SpringMVCProject/src/com/springs/controller/AnnotatedHelloController.java
@@ -1,5 +1,7 @@
 package com.springs.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class AnnotatedHelloController {
 	
 	@RequestMapping("/welcome/{countryName}/{userName}")  //using curly braces helps in mapping the url pattern at the runtime.
-	public ModelAndView helloWorld(@PathVariable("userName") String name,@PathVariable("countryName") String country){//Front controller binds the userName through PathVariable to the 'name' variable.Hence we can use the name variable to sent back request to the client.
+	//public ModelAndView helloWorld(@PathVariable("userName") String name,@PathVariable("countryName") String country){//Front controller binds the userName through PathVariable to the 'name' variable.Hence we can use the name variable to sent back request to the client.
+	public ModelAndView helloWorld(@PathVariable Map<String,String> pathVars){//Not using multiple PathVariable declarations	
+		String name=pathVars.get("userName");
+		String country=pathVars.get("countryName");
 		
 		ModelAndView model=new ModelAndView("HelloPage");
-		model.addObject("msg", "Hello "+name +" From "+country);
+		model.addObject("msg", "Hello "+name +" You are From "+country);
 		
 		return model;
 	}
