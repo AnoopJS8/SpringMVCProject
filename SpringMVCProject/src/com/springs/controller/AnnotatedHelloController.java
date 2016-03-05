fd0d958630ef003b6c447160c550752cae80ee00
AnoopJS8 <anoop.jyoti88@gmail.com>
2016-03-05 16:32:04 -0700

    PathVariable Addition

--- SpringMVCProject/src/com/springs/controller/AnnotatedHelloController.java
+++ SpringMVCProject/src/com/springs/controller/AnnotatedHelloController.java
@@ -1,6 +1,7 @@
 package com.springs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,11 +9,11 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/greet") //adds a relative path to the url
 public class AnnotatedHelloController {
 	
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld(){
+	@RequestMapping("/welcome/{countryName}/{userName}")  //using curly braces helps in mapping the url pattern at the runtime.
+	public ModelAndView helloWorld(@PathVariable("userName") String name,@PathVariable("countryName") String country){//Front controller binds the userName through PathVariable to the 'name' variable.Hence we can use the name variable to sent back request to the client.
 		
 		ModelAndView model=new ModelAndView("HelloPage");
-		model.addObject("msg", "Hello WOrld");
+		model.addObject("msg", "Hello "+name +" From "+country);
 		
 		return model;
 	}
